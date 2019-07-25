c64647697d3a962d34709892a311103048e5a185
Martin Pittermann <martinp2250@gmail.com>
2019-07-25 12:22:53 +0200

    restructure stuff, prob not working

--- database/database.go
+++ database/database.go
@@ -1,62 +1,21 @@
-package minitsdb
+package database
 
 import (
 	"errors"
-	"io/ioutil"
-	"path"
 	"regexp"
 
 	"github.com/martin2250/minitsdb/ingest"
 	"github.com/martin2250/minitsdb/series"
 )
 
-// ServerSession holds the main application data
-type ServerSession struct {
+// Database holds series from a database directory
+type Database struct {
 	Path   string
 	Series []series.Series
 }
 
-// LoadDatabase creates a new server session, loading settings and database from the supplied path
-func LoadDatabase(databasePath string) (ServerSession, error) {
-	s := ServerSession{
-		Path:   databasePath,
-		Series: make([]series.Series, 0),
-	}
-
-	return s, nil
-}
-
-// LoadSeries loads all series from the file system
-func (ss *ServerSession) LoadSeries() error {
-	if len(ss.Series) != 0 {
-		return errors.New("session already has series loaded")
-	}
-
-	files, err := ioutil.ReadDir(ss.Path)
-
-	if err != nil {
-		return err
-	}
-
-	for _, file := range files {
-		if !file.IsDir() {
-			continue
-		}
-
-		s, err := series.OpenSeries(path.Join(ss.Path, file.Name()))
-
-		if err != nil {
-			return err
-		}
-
-		ss.Series = append(ss.Series, s)
-	}
-
-	return nil
-}
-
 // FindSeries returns indices to all series that match the tagset
-func (ss ServerSession) FindSeries(tags map[string]string) []int {
+func (ss Database) FindSeries(tags map[string]string) []int {
 	indices := make([]int, 0)
 
 	for i, series := range ss.Series {
@@ -91,7 +50,7 @@ var ErrSeriesAmbiguous = errors.New("point values matches two series")
 var ErrSeriesUnknown = errors.New("value doesn't match any series")
 
 // InsertPoint finds a matching series and tries to insert the point
-func (ss *ServerSession) InsertPoint(p ingest.Point) error {
+func (ss *Database) InsertPoint(p ingest.Point) error {
 	indices := ss.FindSeries(p.Tags)
 
 	if len(indices) == 0 {
---
+++ database/load.go
@@ -0,0 +1,50 @@
+package database
+
+import (
+	"errors"
+	"io/ioutil"
+	"path"
+
+	"github.com/martin2250/minitsdb/series"
+)
+
+// NewDatabase creates a new database instance
+func NewDatabase(databasePath string) (Database, error) {
+	db := Database{
+		Path:   databasePath,
+		Series: make([]series.Series, 0),
+	}
+
+	err := db.loadSeries()
+
+	return db, err
+}
+
+// LoadSeries loads all series in a database from the file system
+func (db *Database) loadSeries() error {
+	if len(db.Series) != 0 {
+		return errors.New("series already loaded")
+	}
+
+	files, err := ioutil.ReadDir(db.Path)
+
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			continue
+		}
+
+		s, err := series.OpenSeries(path.Join(db.Path, file.Name()))
+
+		if err != nil {
+			return err
+		}
+
+		db.Series = append(db.Series, s)
+	}
+
+	return nil
+}
