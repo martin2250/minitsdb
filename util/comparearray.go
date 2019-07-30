1b3faca9d4753e3d2a3a53a5d93321647a9643cd
Martin Pittermann <martinp2250@gmail.com>
2019-07-30 11:44:19 +0200

    also add compare for uint

--- util/comparearray.go
+++ util/comparearray.go
@@ -24,7 +24,24 @@ func Compare1DUInt64(i1, i2 []uint64) bool {
 	return true
 }
 
-func Compare2DUInt64(i1, i2 [][]int64) bool {
+func Compare2DInt64(i1, i2 [][]int64) bool {
+	if len(i1) != len(i2) {
+		return false
+	}
+	for i := range i1 {
+		if len(i1[i]) != len(i2[i]) {
+			return false
+		}
+		for j := range i1[i] {
+			if i1[i][j] != i2[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func Compare2DUInt64(i1, i2 [][]uint64) bool {
 	if len(i1) != len(i2) {
 		return false
 	}
