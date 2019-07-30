1567bfb6ab79929c3ab481443a97589045b0fc92
Martin Pittermann <martinp2250@gmail.com>
2019-07-30 16:00:04 +0200

    fixed tests

--- database/series/storage/encoder_test.go
+++ database/series/storage/encoder_test.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"bytes"
 	"fmt"
+	"github.com/martin2250/minitsdb/util"
 	"math/rand"
 	"testing"
 	"time"
@@ -11,11 +12,17 @@ import (
 func runEncodeTest(values [][]uint64, times []int64) error {
 	var b bytes.Buffer
 
-	// todo: check if EncodeBlock modifies the input arrays
-	err := EncodeAll(&b, times, values)
+	valuesC := util.Copy2DUInt64(values)
+	timesC := util.Copy1DInt64(times)
+
+	err := EncodeAll(&b, timesC, valuesC)
 
 	if err != nil {
 		return err
+	}
+
+	if !util.Compare1DInt64(times, timesC) || !util.Compare2DUInt64(values, valuesC) {
+		return fmt.Errorf("encoder modified input arrays", b.Len())
 	}
 
 	if b.Len()%4096 != 0 {
@@ -52,16 +59,14 @@ func runEncodeTest(values [][]uint64, times []int64) error {
 	return nil
 }
 
-func BenchmarkEncoder(b *testing.B) {
-	rand.Seed(time.Now().UnixNano())
-
-	values := make([][]int64, 8)
+func createData(n, N, d int) ([][]uint64, []int64) {
+	values := make([][]int64, n)
 
 	for i := range values {
-		values[i] = make([]int64, 2000)
-		last := rand.Int63n(600)
+		values[i] = make([]int64, N)
+		last := rand.Int63n(d)
 		for j := range values[i] {
-			last = last + rand.Int63n(50) - 25
+			last = last + rand.Int63n(d) - int64(d/2)
 			values[i][j] = last
 		}
 	}
@@ -71,13 +76,21 @@ func BenchmarkEncoder(b *testing.B) {
 		var err error
 		valuesE[i], err = DiffTransformer{N: 1}.Apply(values[i])
 		if err != nil {
-			b.Errorf("transformer encountered error: %v", err)
-			return
+			panic(err)
 		}
 	}
+
+	return valuesE, values[0]
+}
+
+func BenchmarkEncoder(b *testing.B) {
+	rand.Seed(time.Now().UnixNano())
+
+	values, time := createData(10, 1000, 500)
+
 	b.StartTimer()
 	for n := 0; n < b.N; n++ {
-		err := runEncodeTest(valuesE, values[0])
+		err := runEncodeTest(values, time)
 		if err != nil {
 			b.Error(err)
 			return
@@ -89,27 +102,9 @@ func BenchmarkEncoder(b *testing.B) {
 func TestEncoder(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 
-	values := make([][]int64, 8)
+	values, time := createData(10, 1000, 500)
 
-	for i := range values {
-		values[i] = make([]int64, 200)
-
-		for j := range values[i] {
-			values[i][j] = rand.Int63n(2525323)
-		}
-	}
-
-	valuesE := make([][]uint64, len(values))
-	for i := range values {
-		var err error
-		valuesE[i], err = DiffTransformer{N: 1}.Apply(values[i])
-		if err != nil {
-			t.Errorf("transformer encountered error: %v", err)
-			return
-		}
-	}
-
-	err := runEncodeTest(valuesE, values[0])
+	err := runEncodeTest(values, time)
 
 	if err != nil {
 		t.Error(err)
